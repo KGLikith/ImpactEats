6c4f09ac83a81626a720421d081da14b279a8cfa
KGLikith <glikith1705@gmail.com>
2025-01-06 14:16:06 +0530

    redirecting to profile

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 
 export default function page() {
+  const router = useRouter();
+  router.push('/profile')
   return (
     <div>page</div>
   )
