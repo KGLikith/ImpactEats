e5bd834a67c8ddc080ffbe638c93a2231d8ddbd2
KGLikith <glikith1705@gmail.com>
2025-01-06 11:07:27 +0530

    packages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["ucarecdn.com"],
   },
+  // install packages forcing to use the same version as the one in th
 };
 
 export default nextConfig;
